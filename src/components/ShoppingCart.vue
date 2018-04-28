a184f6c3a8644212d95a8c46e38225f24fedc0a6
Reynaldo Barros Jr <jamjrs@gmail.com>
2018-04-28 11:18:16 -0300

    Checkout terminado

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-      <h1>Carrinho</h1>
+      <h1>Carrinhos</h1>
       <ul>
         <li v-for="product in products">
           {{product.title}} - {{product.price | currency}} - {{product.quantity}}
         </li>
       </ul>
       <p>Total: {{total | currency}}</p>
+      <button @click="$store.dispatch('checkout')" name="btncheckout">Checkout</button>
+      <p v-if="$store.state.checkoutStatus">{{$store.state.checkoutStatus}}</p>
     </div>
 </template>
 
