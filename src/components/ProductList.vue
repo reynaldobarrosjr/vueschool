fb30614faff74709ae24a161acbc66e52c3afa92
Reynaldo Barros Jr <jamjrs@gmail.com>
2018-04-28 09:50:33 -0300

    antes de continuar com a class de checkout

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <h1>Product List</h1>
+      <h1>Lista de Produtos</h1>
       <img
         v-if="loading"
         src="https://i.imgur.com/JfPpwOA.gif"
@@ -8,7 +8,7 @@
       <ul v-else>
         <li v-for="product in products">
           {{product.title}} - {{product.price | currency}} - {{product.inventory}}
-          <button @click="addProductToCart(product)">Add to cart</button>
+          <button @click="addProductToCart(product)">Adicionar ao carrinho</button>
         </li>
       </ul>
     </div>
--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <h1>Shopping Cart</h1>
+      <h1>Carrinho</h1>
       <ul>
         <li v-for="product in products">
           {{product.title}} - {{product.price | currency}} - {{product.quantity}}
